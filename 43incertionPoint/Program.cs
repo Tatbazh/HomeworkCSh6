a6175d0092af2332323dd4a7c1459c2b329f1b68
Tatyana <tatyana.bazh17@gmail.com>
2022-10-20 22:46:07 +0400

    решены задачи д/з 41 и 43 по С# семинар №6

--- 43incertionPoint/Program.cs
+++ 43incertionPoint/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 43. Напишите программу, которая найдёт точку
+// пересечения двух прямых, заданных уравнениями 
+//y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются
+// пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+Console.Write("Введите значения b1, k1, b2, k2 через запятую и пробел: ");
+string arrayNumbers = Console.ReadLine()!;
+
+int[] GetArrayFromString(string stringArray)
+{
+    string[] StringNumbers = stringArray.Split(", ", StringSplitOptions.RemoveEmptyEntries);
+    int[] arrayNew = new int[StringNumbers.Length];
+    for (int i = 0; i < StringNumbers.Length; i++)
+    {
+        arrayNew[i] = int.Parse(StringNumbers[i]);
+    }
+    return arrayNew;
+}
+
+void FormulaLineIntersection(int[] arrayNumbers)
+{
+    int B1 = arrayNumbers[0]; int K1 = arrayNumbers[1]; int B2 = arrayNumbers[2]; int K2 = arrayNumbers[3];
+    double X = 0; double Y = 0;      // X и Y высчитываем из формул: Y = (K1 * X + B1); Y = (K2 * X + B2);
+    
+    X = (double)(B1 - B2) / (K2 - K1);  
+    Y = K1 * X + B1;
+    if ((Y - K1 * X - B1 != 0) && (Y - K2 * X - B2 != 0))  Console.WriteLine("Прямые параллельны");
+    else Console.WriteLine($"Прямые пересекаются в точке ({X}, {Y})");
+}
+int[] valueArray = GetArrayFromString(arrayNumbers);
+Console.WriteLine($"{String.Join(" ", valueArray)}");  // выводим массив, элементы к-го разделены пробелом
+FormulaLineIntersection(valueArray);
---
+++ TaskStar/Program.cs
@@ -0,0 +1,47 @@
+﻿// Задача со звездочкой. Напишите программу, которая
+// реализует обход введенного двумерного массива,
+// начиная с крайнего нижнего левого элемента против
+// часовой стрелки.
+// 1 2 3
+// 4 5 6 -> 7 8 9 6 3 2 1 4 5
+// 7 8 9
+void RandomArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+            array[i, j] = new Random().Next(1, 10);
+    }
+}
+int[,] matrix = new int[3, 3];
+
+void PrintArray(int[,] table)
+{
+    for (int i = 0; i < table.GetLength(0); i++)
+    {
+        for (int j = 0; j < table.GetLength(1); j++)
+        {
+            Console.Write($"{table[i, j]}");
+        }
+        Console.WriteLine();
+    }
+}
+RandomArray(matrix);
+PrintArray(matrix); // заполнили массив случайными числами, вывели все в консоль
+
+int[] FillImage(int row, int col)
+{    
+    if (array2[row, col] == true)
+    {
+        array2[row, col] = array1[i, j];
+        FillImage(row, col + 1); // здесь метод сам себя вызывает
+        FillImage(row - 1, col);
+        FillImage(row, col - 1);
+        FillImage(row - 1, col);
+    }
+    return arra1;
+}
+
+FillImage(2, 1); // [2,1] - начальная точка
+int[,] newArray = FillImage;
+Console.WriteLine();
